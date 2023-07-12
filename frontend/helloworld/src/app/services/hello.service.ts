18ad3532665a7778e8616bd2d9c9131ca7f1504d
hostingtestdev <hostingtestdev@proton.me>
2023-07-12 02:44:00 -0400

    adsf

--- frontend/helloworld/src/app/services/hello.service.ts
+++ frontend/helloworld/src/app/services/hello.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const API_URL = 'http://localhost:8080/api/v1/';
+//const API_URL = 'http://localhost:8080/api/v1/';
+const API_URL = 'http://hostingtest-v3:8080/api/v1/';
 
 @Injectable({
   providedIn: 'root',
